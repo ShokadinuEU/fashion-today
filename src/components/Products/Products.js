977f88916cd807c41bcfdb261be7b049601abdfd
Timotei Dumitru <dumitru.timy@gmail.com>
2018-10-04 21:01:02 +0100

    few more fixes on items list

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,30 +1,49 @@
 import React, { Component } from 'react'
-// import dataMen from '../Products/data/mendata.json'
 import './products.css'
+import axios from 'axios';
+
 
 class Products extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
+      isLoading: false,
+      error: null
     };
   }
 
   componentDidMount(){
-    fetch('./mendata.json')
-      .then(response => response.json())
-      .then(data => this.setState({ data }))
+    this.setState({ isLoading: true });
+    axios.get('./mendata.json')
+      .then(result => this.setState({
+        data: result.data,
+        isLoading: false 
+      }))
+      .catch(error => this.setState({ 
+        error, 
+        isLoading: false 
+      }));
   }
 
   render() {
-    const { data } = this.state;
+    const { data, isLoading, error } = this.state
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (isLoading) {
+      return <p>Loading ...</p>
+    }
+
     return (
       <div className="products-main">
       <h1 className="products-header">-----Latest Products-----</h1>
       <div className="products-wrapper">
         {data.map(item =>
           <div key={item.id} className="item">
-            <span className="item-detail">{item.image}</span>
+            <div className="item-image">Image here</div>
             <span className="item-detail">{item.name}</span>
             <span className="item-detail">{item.price}</span>
             <span className="item-detail">{item.sold}</span>
