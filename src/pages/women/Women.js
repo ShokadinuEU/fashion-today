bef681d64da99c659de4a6ede8f4dfb74d55281c
Timotei Dumitru <dumitru.timy@gmail.com>
2018-10-08 22:41:18 +0100

    implemented the content on women page and style it aswell

--- src/pages/women/Women.js
+++ src/pages/women/Women.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Products from '../women/WomenContent'
 
 export default class Women extends Component {
   render() {
     return (
-      <div className="main-content">
+      <div>
+        <Products />
       </div>
     )
   }
---
+++ src/pages/women/WomenContent.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import './women_content.scss'
+import axios from 'axios';
+
+
+class Products extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      isLoading: false,
+      error: null
+    };
+  }
+
+  componentDidMount(){
+    this.setState({ isLoading: true });
+    axios.get('./men.json')
+      .then(result => this.setState({
+        data: result.data,
+        isLoading: false 
+      }))
+      .catch(error => this.setState({ 
+        error, 
+        isLoading: false 
+      }));
+  }
+
+  render() {
+    const { data, isLoading, error } = this.state
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (isLoading) {
+      return <p>Loading ...</p>
+    }
+
+    return (
+      <div className="products-main">
+        <h1 className="products-header" id="hot-items">-----Latest Products-----</h1>
+        <hr className="style-eight"/>
+        <div className="products-wrapper">
+          {data.map(item =>
+            <div key={item.id} className="women">
+              <div className="women-image"></div>
+              <span className="women-detail">{item.name}</span>
+              <span className="women-detail">{item.price}</span>
+              <button className="buy-it">Buy It</button>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+export default Products;
