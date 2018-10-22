46cc129fe13a25a141a06b2ed49c33766bf2ce0e
Timotei Dumitru <dumitru.timy@gmail.com>
2018-10-22 11:04:23 +0100

    few more fixes on pages section

--- src/pages/kids/Kids.js
+++ src/pages/kids/Kids.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react'
-import KidsContent from './KidsContent'
+import './kids_content.scss'
+import axios from 'axios';
 
-class About extends Component {
+
+class MenContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      isLoading: false,
+      error: null
+    };
+  }
+
+  componentDidMount(){
+    this.setState({ isLoading: true });
+    axios.get('./kids.json')
+      .then(result => this.setState({
+        data: result.data,
+        isLoading: false 
+      }))
+      .catch(error => this.setState({ 
+        error, 
+        isLoading: false 
+      }));
+  }
+
   render() {
+    const { data, isLoading, error } = this.state
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (isLoading) {
+      return <p>Loading ...</p>
+    }
+
     return (
-      <div className="main-content">
-        <KidsContent />
+      <div className="products-main">
+        <h1 className="products-header">-----Latest Products-----</h1>
+        <hr className="style-eight"/>
+        <div className="products-wrapper">
+          {data.map(item =>
+            <div key={item.id} className="kids">
+              <div className="kids-image"></div>
+              <span className="kids-detail">{item.name}</span>
+              <span className="kids-detail">{item.price}</span>
+              <button className="buy-it">Buy It</button>
+            </div>
+          )}
+        </div>
       </div>
-    )
+    );
   }
 }
-
-export default About
+export default MenContent;
