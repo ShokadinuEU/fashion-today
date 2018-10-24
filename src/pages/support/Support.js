0166239fed52f33a19a28a27ac34d8ea10322c57
Timotei Dumitru <dumitru.timy@gmail.com>
2018-10-24 23:26:44 +0100

    Almost finish to style form on support page

--- src/pages/support/Support.js
+++ src/pages/support/Support.js
@@ -10,16 +10,16 @@ function Support() {
           <form id="submit-ticket" className="submit-form">
             <p>Simply complete the form and click 'send' to submit an enquiry:</p>
             <div>
-              <div>
+              <div className="label-name">
                 <label>Full Name * </label><br />
                 <input type="text"/>
               </div>
-              <div>
+              <div className="label-email">
                 <label>Email * </label><br />
                 <input type="text"/>
                 <p>Please ensure that your email address is correct so that we can respond to your enquiry.</p>
-              </div>
-              <div>
+              </div><br/>
+              <div className="contact-preference">
                 <label>Contact Preferences *</label><br />
                 <select name="contact-preference">
                 <option value="">Contact Preferences</option>
@@ -27,8 +27,8 @@ function Support() {
                 <option value="2">If you need to, you may contact me by phone</option>
                 <option value="3">I don’t want to be contacted by phone</option>
                 </select>
-              </div>
-              <div>
+              </div><br/>
+              <div className="shipping-address">
                 <label>Location(Shipping Address) *</label><br />
                 <select name="Location(Shipping Address)">
                 <option value="">Location(Shipping Address)</option>
@@ -38,7 +38,7 @@ function Support() {
                 <option value="3">Aus</option>
                 <option value="3">Asia</option>
                 </select>
-              </div>
+              </div><br/>
               <div>
                 <label>Query Type(select) *</label><br />
                 <select name="Query Type(select)">
@@ -49,20 +49,20 @@ function Support() {
                 <option value="3">My Order</option>
                 <option value="3">Returns</option>
                 </select>
-              </div>
-              <div>
+              </div><br/>
+              <div className="subject-form">
                 <label>Subject * </label><br />
                 <input type="text"/>
-              </div>
-              <div>
+              </div><br/>
+              <div className="message-form">
                 <label>Messaje * </label><br />
-                <textarea name="" id="" cols="40" rows="4"></textarea>
-              </div>
+                <textarea name="" id="" cols="37" rows="5"></textarea>
+              </div><br/>
               <div> 
                 <label for="Attachment">Upload File (Optional) (RECOMMENDED)</label> 
                 <input data-val="true" data-val-accept="Invalid File Type" data-val-accept-exts="jpg,jpeg,gif,png,tif,tiff,txt,doc,docx,pdf" data-val-filesize="You have exceeded the maximum file size 4 Mb" data-val-filesize-maxsize="4194304" id="Attachment" name="Attachment" placeholder="Name" tabindex="1" value="" type="file" />
               </div>
-              <div>
+              <div className="submit-form-button">
                 <br/>
                 <button id="btnSendContactUs">Send</button> 
               </div>
