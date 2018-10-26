33c36512682133c383675ebb88f580f97f5421a1
Timotei Dumitru <dumitru.timy@gmail.com>
2018-10-26 19:12:21 +0100

    few more fixes for responsiveness on large screens

--- src/pages/support/Email.js
+++ src/pages/support/Email.js
@@ -4,11 +4,11 @@ import React from 'react';
 const Email = (props) => {
 	return (  
   <div className="form-group">
-    <label for={props.name} className="form-label">{props.title}</label>
+    <label for={props.email} className="form-label">{props.title}</label>
     <input
       className="form-control"
-      id={props.name}
-      name={props.name}
+      id={props.email}
+      name={props.email}
       type={props.inputType}
       value={props.value}
       onChange={props.handleChange}
